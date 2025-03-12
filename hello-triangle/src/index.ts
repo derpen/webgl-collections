223e1a1a3e4114d48e2c0ec072a5920ced6f9c65
derpen <rizqyassabil@hotmail.com>
2025-03-12 16:22:53 +0700

    Actually triangle in webgl with typescript, waow

--- hello-triangle/src/index.ts
+++ hello-triangle/src/index.ts
@@ -1,3 +1,50 @@
-document.addEventListener('DOMContentLoaded', (_event) => {
+const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+const gl: WebGLRenderingContext = canvas.getContext('webgl') as WebGLRenderingContext;
 
-});
+const vertices: Float32Array = new Float32Array([
+  -0.5, -0.5, 0.0,
+   0.5, -0.5, 0.0,
+   0.0,  0.5, 0.0
+]);
+const vbo: WebGLBuffer = gl.createBuffer() as WebGLBuffer;
+gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+const vertexShaderCode: string = `
+  attribute vec3 position;
+
+  void main(){
+    gl_Position = vec4(position, 1.0);
+  }
+`;
+
+const fragmentShaderCode: string = `
+  precision mediump float;
+
+  void main(){
+    vec3 color = vec3(1.0, 0.0, 0.0);
+    gl_FragColor = vec4(color, 1.0);
+  }
+`;
+
+const vertexShader: WebGLShader = gl.createShader(gl.VERTEX_SHADER) as WebGLShader;
+gl.shaderSource(vertexShader, vertexShaderCode);
+gl.compileShader(vertexShader);
+
+const fragmentShader: WebGLShader = gl.createShader(gl.FRAGMENT_SHADER) as WebGLShader;
+gl.shaderSource(fragmentShader, fragmentShaderCode);
+gl.compileShader(fragmentShader);
+
+const program: WebGLProgram = gl.createProgram() as WebGLProgram;
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+gl.useProgram(program);
+
+const positionLocation: number = gl.getAttribLocation(program, 'position');
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(positionLocation);
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
